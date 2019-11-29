a6ea77311765826fcb201c97c4a98dec3a575fc9
Lars Vilhuber <lars.vilhuber@cornell.edu>
2019-11-29 11:04:51 -0500

    Create install.R

---
+++ install.R
@@ -0,0 +1,22 @@
+####################################
+# global libraries used everywhere #
+####################################
+
+mran.date <- "2019-10-01"
+options(repos=paste0("https://cran.microsoft.com/snapshot/",mran.date,"/"))
+
+
+
+pkgTest <- function(x)
+{
+	if (!require(x,character.only = TRUE))
+	{
+		install.packages(x,dep=TRUE)
+		if(!require(x,character.only = TRUE)) stop("Package not found")
+	}
+	return("OK")
+}
+
+global.libraries <- c("data.table","dplyr","devtools","rprojroot","tictoc","knitr","kableExtra","stringr","rcrossref","RefManageR","ggplot2","ggpubr","tools","forcats")
+
+results <- sapply(as.list(global.libraries), pkgTest)
